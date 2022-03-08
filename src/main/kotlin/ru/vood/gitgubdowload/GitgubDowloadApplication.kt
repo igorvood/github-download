d9f3bf05315fbab7c72d24350e920977ccc3c676
vood <balitsky_igor@list.ru>
2022-03-08 11:34:58 +0300

    init

---
+++ src/main/kotlin/ru/vood/gitgubdowload/GitgubDowloadApplication.kt
@@ -0,0 +1,11 @@
+package ru.vood.gitgubdowload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GitgubDowloadApplication
+
+fun main(args: Array<String>) {
+    runApplication<GitgubDowloadApplication>(*args)
+}
---
+++ src/test/kotlin/ru/vood/gitgubdowload/GitgubDowloadApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vood.gitgubdowload
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GitgubDowloadApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
